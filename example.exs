e10f3caaa985503ffe370c72cbb9d7089acc3c91
songsong <songsong@Gialib001deMini.lan>
2016-09-18 18:05:40 +0800

    example.exs

--- example.exs
+++ example.exs
@@ -4,3 +4,10 @@ defmodule Example do
   @attr "two"
   def second, do: @attr
 end
+
+# MF7
+:io.format("~.5f~n", [2.33211231]) |> to_string |>  is_binary |> IO.puts
+
+IO.puts System.get_env "HOME"
+IO.puts Path.extname "../example.exs"
+IO.puts System.cwd
